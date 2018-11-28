4ace92f960870acd25a9e93fb5cabc74c53d5076
Rachael Stedman <rkstedman@users.noreply.github.com>
2018-11-28 23:07:24 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "www.rkstedman.com/precache-manifest.14545c001fe5ba881dda065520c31635.js"
+  "/precache-manifest.594a47aa9f4ff9f03da2510c1007ce46.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("www.rkstedman.com/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
